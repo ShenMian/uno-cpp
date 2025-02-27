c9f51562e09fd9172fac07179bba1d78e4597d5f
ShenMian <sms_school@outlook.com>
2025-02-27 14:47:03 +0000

    refactor(deck): remove unused includes and update main.cpp
    - Remove unused #include <random> from deck.hpp
    - Add #include <vector> to main.cpp

--- src/deck.hpp
+++ src/deck.hpp
@@ -4,7 +4,6 @@
 #include <cassert>
 #include <memory>
 #include <optional>
-#include <random>
 #include <vector>
 
 #include "card.hpp"
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
 
 #include "deck.hpp"
 
