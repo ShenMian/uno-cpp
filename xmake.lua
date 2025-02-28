5f31f008b253e51b59cd51dd1556cc2307575465
ShenMian <sms_school@outlook.com>
2025-02-28 01:31:05 +0000

    build(xmake): add debug symbols for development
    - Set debug symbols when in debug mode for better development experience

--- xmake.lua
+++ xmake.lua
@@ -2,6 +2,10 @@ add_requires("sfml 3.0.0")
 
 set_languages("c++20")
 
+if is_mode("debug") then
+    set_symbols("debug")
+end
+
 target("uno")
     set_kind("binary")
     set_warnings("all", "error")
