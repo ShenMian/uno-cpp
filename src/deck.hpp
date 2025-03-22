5731e52591cf1c6ec6a386746bd61367efcc5c20
ShenMian <sms_school@outlook.com>
2025-03-22 13:57:48 +0000

    feat(ai): implement basic AI and game logic
    - Add AIPlayer class with simple AI for playing cards and choosing colors
    - Implement core game logic including turns, actions, and win conditions
    - Refactor Deck class to use a random number generator for shuffling
    - Update main loop to use a timer for turn updates

--- src/deck.hpp
+++ src/deck.hpp
@@ -5,6 +5,7 @@
 #include <cassert>
 #include <memory>
 #include <optional>
+#include <random>
 #include <vector>
 
 #include "card.hpp"
@@ -17,46 +18,15 @@ using std::vector;
 class Deck {
   public:
     /// Constructs a deck with all 108 UNO cards.
-    Deck() {
-        // UNO includes 108 cards: 25 in each of four color suits (red, yellow,
-        // green, blue), each suit consisting of one zero, two each of 1
-        // through 9, and two each of the action cards "Skip", "Draw Two", and
-        // "Reverse". The deck also contains four "Wild" cards and four "Wild
-        // Draw Four".
+    Deck(std::mt19937 rng) : rng_(rng) {
         cards_.reserve(108);
-        for (const auto color :
-             {Color::Red, Color::Blue, Color::Green, Color::Yellow}) {
-            cards_.push_back(std::make_unique<NumberCard>(color, 0));
-            for (uint8_t number = 1; number <= 9; number++) {
-                cards_.push_back(std::make_unique<NumberCard>(color, number));
-                cards_.push_back(std::make_unique<NumberCard>(color, number));
-            }
-            for (const auto symbol :
-                 {ActionSymbol::DrawTwo,
-                  ActionSymbol::Reverse,
-                  ActionSymbol::Skip}) {
-                cards_.push_back(std::make_unique<ActionCard>(color, symbol));
-                cards_.push_back(std::make_unique<ActionCard>(color, symbol));
-            }
-        }
-        for (int i = 0; i < 4; i++) {
-            cards_.push_back(std::make_unique<WildCard>(WildSymbol::Wild));
-            cards_.push_back(std::make_unique<WildCard>(WildSymbol::WildDrawFour
-            ));
-        }
-        assert(cards_.size() == 108);
-    }
-
-    /// Shuffles the deck.
-    template<typename Rng>
-    void shuffle(Rng& rng) {
-        std::ranges::shuffle(cards_, rng);
+        initialize_cards();
     }
 
     /// Draws a card from the deck.
     optional<unique_ptr<Card>> draw() noexcept {
         if (cards_.empty()) {
-            return std::nullopt;
+            initialize_cards();
         }
         auto card = std::move(cards_.back());
         cards_.pop_back();
@@ -74,5 +44,41 @@ class Deck {
     }
 
   private:
+    void initialize_cards() {
+        // UNO includes 108 cards: 25 in each of four color suits (red, yellow,
+        // green, blue), each suit consisting of one zero, two each of 1
+        // through 9, and two each of the action cards "Skip", "Draw Two", and
+        // "Reverse". The deck also contains four "Wild" cards and four "Wild
+        // Draw Four".
+        for (const auto color :
+             {Color::Red, Color::Blue, Color::Green, Color::Yellow}) {
+            cards_.push_back(std::make_unique<NumberCard>(color, 0));
+            for (uint8_t number = 1; number <= 9; number++) {
+                cards_.push_back(std::make_unique<NumberCard>(color, number));
+                cards_.push_back(std::make_unique<NumberCard>(color, number));
+            }
+            for (const auto symbol :
+                 {ActionSymbol::DrawTwo,
+                  ActionSymbol::Reverse,
+                  ActionSymbol::Skip}) {
+                cards_.push_back(std::make_unique<ActionCard>(color, symbol));
+                cards_.push_back(std::make_unique<ActionCard>(color, symbol));
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            cards_.push_back(std::make_unique<WildCard>(WildSymbol::Wild));
+            cards_.push_back(
+                std::make_unique<WildCard>(WildSymbol::WildDrawFour)
+            );
+        }
+        shuffle();
+    }
+
+    /// Shuffles the deck.
+    void shuffle() {
+        std::ranges::shuffle(cards_, rng_);
+    }
+
     vector<unique_ptr<Card>> cards_;
+    std::mt19937 rng_;
 };
--- src/state.hpp
+++ src/state.hpp
@@ -1,31 +1,77 @@
+#include <cassert>
+#include <cstdint>
+#include <memory>
 #include <random>
 #include <vector>
 
+#include "card.hpp"
 #include "deck.hpp"
 #include "discard_pile.hpp"
 #include "player.hpp"
 
-enum class Direction { Clockwise = 1, CounterClockwise = -1 };
+enum class Direction : int8_t { Clockwise = 1, CounterClockwise = -1 };
 
 class State {
   public:
-    State() : seed_(std::random_device {}()), rng_(seed_) {
-        deck_.shuffle(rng_);
-
+    State() : seed_(std::random_device {}()), rng_(seed_), deck_(rng_) {
         for (const auto position :
-             {Position::North, Position::East, Position::South, Position::West
-             }) {
-            players_.emplace_back(position, deck_);
+             {Position::South,
+              Position::West,
+              Position::North,
+              Position::East}) {
+            players_.push_back(std::make_unique<AiPlayer>(position, deck_));
         }
 
-        // FIXME(ShenMian): Skip wild card for now.
-        while (auto card = deck_.draw().value()) {
-            if (dynamic_cast<const WildCard*>(card.get()) != nullptr) {
-                continue;
-            }
-            discard_pile_.push_back(std::move(card));
-            break;
+        auto card = deck_.draw().value();
+        if (auto wild_card = dynamic_cast<WildCard*>(card.get())) {
+            wild_card->set_color(current_player()->choose_color());
         }
+        discard_pile_.push_back(std::move(card));
+    }
+
+    void update() {
+        auto player = current_player();
+        next_turn();
+
+        auto card = player->play_card(discard_pile_);
+        while (!card.has_value()) {
+            player->draw_card(deck_);
+            card = player->play_card(discard_pile_);
+        }
+        if (player->is_hand_empty()) {
+            assert(false); // TODO
+        }
+        if (auto wild_card = dynamic_cast<WildCard*>(card.value().get())) {
+            wild_card->set_color(player->choose_color());
+            if (wild_card->symbol() == WildSymbol::WildDrawFour) {
+                auto next_player = current_player();
+                next_player->draw_card(deck_);
+                next_player->draw_card(deck_);
+                next_player->draw_card(deck_);
+                next_player->draw_card(deck_);
+                next_turn();
+            }
+        }
+        if (auto action_card =
+                dynamic_cast<const ActionCard*>(card.value().get())) {
+            switch (action_card->symbol()) {
+                case ActionSymbol::DrawTwo: {
+                    auto next_player = current_player();
+                    next_player->draw_card(deck_);
+                    next_player->draw_card(deck_);
+                    next_turn();
+                    break;
+                }
+                case ActionSymbol::Reverse:
+                    reverse_direction();
+                    break;
+                case ActionSymbol::Skip:
+                    next_turn();
+                    break;
+            }
+        }
+        assert(card.value()->can_play_on(discard_pile_.peek_top()));
+        discard_pile_.push_back(std::move(card.value()));
     }
 
     void reverse_direction() {
@@ -43,18 +89,27 @@ class State {
         deck_.render(render_target);
         discard_pile_.render(render_target);
         for (const auto& player : players_) {
-            player.render(render_target, discard_pile_);
+            player->render(render_target, discard_pile_);
         }
     }
 
   private:
-    Deck deck_;
-    DiscardPile discard_pile_;
+    Player* current_player() {
+        return players_[player_index_].get();
+    }
 
-    std::vector<Player> players_;
-    [[maybe_unused]] size_t player_index_ = 0;
-    Direction direction_ = Direction::Clockwise;
+    void next_turn() {
+        player_index_ =
+            (player_index_ + static_cast<int8_t>(direction_)) % players_.size();
+    }
 
     unsigned int seed_;
     std::mt19937 rng_;
+
+    Deck deck_;
+    DiscardPile discard_pile_;
+
+    std::vector<std::unique_ptr<Player>> players_;
+    uint8_t player_index_ = 0;
+    Direction direction_ = Direction::Clockwise;
 };
