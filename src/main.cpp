6e2be0338028d7bce437277f9ee6b0498c71cfc0
ShenMian <sms_school@outlook.com>
2025-03-28 19:10:27 +0000

    feat(main): implement multithreading for state updates
    - Add a new thread for state updates
    - Move state update logic to the new thread
    - Detach the thread to allow asynchronous execution

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <thread>
 
 #include "state.hpp"
 
@@ -8,22 +9,29 @@ int main() {
 
     State state;
 
+    std::thread thread([&]() {
+        while (window.isOpen()) {
+            static sf::Clock clock;
+            if (clock.getElapsedTime().asSeconds() > 1.5f) {
+                state.update();
+                clock.restart();
+            }
+        }
+    });
+    thread.detach();
+
     while (window.isOpen()) {
         while (const auto event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>()) {
                 window.close();
             }
             if (auto resized = event->getIf<sf::Event::Resized>()) {
-                window.setView(sf::View(
-                    sf::FloatRect({0.0f, 0.0f}, sf::Vector2f(resized->size))
-                ));
+                window.setView(
+                    sf::View(
+                        sf::FloatRect({0.0f, 0.0f}, sf::Vector2f(resized->size))
+                    )
+                );
             }
-        }
-
-        static sf::Clock clock;
-        if (clock.getElapsedTime().asSeconds() > 1.5f) {
-            state.update();
-            clock.restart();
         }
 
         window.clear();
