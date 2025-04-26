8fd8500f956bee975d0fbd30531adf99004d0070
ShenMian <sms_school@outlook.com>
2025-04-26 01:17:22 +0100

    refactor(button): rename shape variable to shape_

--- src/button.hpp
+++ src/button.hpp
@@ -5,10 +5,10 @@
 
 class Button {
   public:
-    Button(std::unique_ptr<sf::Shape> shape) : shape(std::move(shape)) {}
+    Button(std::unique_ptr<sf::Shape> shape) : shape_(std::move(shape)) {}
 
     bool is_hovered(sf::RenderWindow& window) const {
-        return shape->getGlobalBounds().contains(
+        return shape_->getGlobalBounds().contains(
             window.mapPixelToCoords(sf::Mouse::getPosition(window))
         );
     }
@@ -18,19 +18,27 @@ class Button {
             && sf::Mouse::isButtonPressed(sf::Mouse::Button::Left);
     }
 
-    void render(sf::RenderWindow& window) const {
+    virtual void set_position(sf::Vector2f position) {
+        shape_->setPosition(position);
+    }
+
+    const sf::Shape& shape() const {
+        return *shape_;
+    }
+
+    virtual void render(sf::RenderWindow& window) const {
         constexpr float HIGHLIGHT_THICKNESS = 4.0f;
 
         if (is_hovered(window)) {
-            shape->setOutlineThickness(HIGHLIGHT_THICKNESS);
-            shape->setOutlineColor(sf::Color::Yellow);
+            shape_->setOutlineThickness(HIGHLIGHT_THICKNESS);
+            shape_->setOutlineColor(sf::Color::Yellow);
         } else {
-            shape->setOutlineThickness(HIGHLIGHT_THICKNESS);
-            shape->setOutlineColor(sf::Color::Black);
+            shape_->setOutlineThickness(HIGHLIGHT_THICKNESS);
+            shape_->setOutlineColor(sf::Color::Black);
         }
-        window.draw(*shape);
+        window.draw(*shape_);
     }
 
   private:
-    std::unique_ptr<sf::Shape> shape;
+    std::unique_ptr<sf::Shape> shape_;
 };
